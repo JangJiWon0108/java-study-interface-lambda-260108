b2978cfc9e1d2a9ca24fd4cc2e4af3ac283776ef
jiwon <jangjw01088@naver.com>
2026-01-06 16:39:54 +0900

    for sort

--- src/main/java/com/jjw/study/service/impl/SortServiceImpl.java
+++ src/main/java/com/jjw/study/service/impl/SortServiceImpl.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +14,8 @@ public class SortServiceImpl implements SortService {
 
     /**
      * for 이용한 정렬
-     * @param sortTarget (key or value)
-     * @param sortOrder (asc or desc)
+     * @param sortTarget (KEY or VALUE)
+     * @param sortOrder (ASC or DESC)
      * @return
      */
     public List<Map<Integer, Integer>> sortUsingFor(String sortTarget, String sortOrder) {
@@ -26,23 +23,36 @@ public class SortServiceImpl implements SortService {
         List<Map<Integer, Integer>> resultList = generateRandomList(1, 100, 100);
 
         for (int i = 0; i < resultList.size() - 1; i = i + 1) {
+            for (int j = 0; j < resultList.size() - 1 - j; j = j + 1) {
+                // 현재 Map, 다음 Map 추출
+                Map<Integer, Integer> currentMap = resultList.get(j);
+                Map<Integer, Integer> nextMap = resultList.get(j + 1);
+                Integer currentTarget;
+                Integer nextTarget;
 
-        }
+                // sortTarget 에 따라 key, value 추출
+                if (sortTarget.equals("KEY")) {
+                    currentTarget = currentMap.entrySet().iterator().next().getKey();
+                    nextTarget = nextMap.entrySet().iterator().next().getKey();
+                } else {
+                    currentTarget = currentMap.entrySet().iterator().next().getValue();
+                    nextTarget = nextMap.entrySet().iterator().next().getValue();
+                }
 
-        switch (sortTarget) {
-            case "KEY" -> {
-                switch (sortOrder) {
-                    case "ASC" -> {
-
+                // sortOrder 에 따라 비교 및 교체
+                if (sortOrder.equals("ASC")) {
+                    if (currentTarget > nextTarget) {
+                        resultList.set(j, nextMap);
+                        resultList.set(j + 1, currentMap);
                     }
-                    case "DESC" -> log.info("a");
+                } else {
+                    if (currentTarget < nextTarget) {
+                        resultList.set(j, nextMap);
+                        resultList.set(j + 1, currentMap);
+                    }
                 }
-            }
-            case "VALUE" -> {
-                switch (sortOrder) {
-                    case "ASC" -> log.info("bdb");
-                    case "DESC" -> log.info("zdz");
-                }
+
+
             }
         }
 
